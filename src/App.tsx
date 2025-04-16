cb6311e51ea813193ab2394f6d54c95a8ebd85bd
songhj10207@naver.com <songhj10207@naver.com>
2025-04-16 11:01:19 +0900

    fix: tailwind 라이브러리 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,7 @@
-import './index.css';
+import { RouterProvider } from 'react-router-dom';
+import { router } from './router';
 
 function App() {
-  return (
-    <div className="w-[100vw] flex justify-center items-center">
-      <main className="max-w-[600px] font-sans h-screen bg-neutral text-primary flex flex-col justify-center items-center">
-        <h1 className="text-4xl font-bold mb-5">웹앱 스타일로 보이게 하기</h1>
-        <button className="px-6 py-3 bg-primary text-white rounded-lg shadow-md hover:shadow-lg">
-          클릭하세요
-        </button>
-      </main>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
-
 export default App;
---
+++ src/pages/HomePage.tsx
@@ -0,0 +1,12 @@
+const HomePage = () => {
+  return (
+    <div>
+      <main>
+        <h2>안녕하세요 민지님!</h2>
+        <p>안녕</p>
+      </main>
+    </div>
+  );
+};
+
+export default HomePage;

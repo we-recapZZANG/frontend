b3dda3972a138256cc68d0382c9b6ea48a41bd7d
songhj10207@naver.com <songhj10207@naver.com>
2025-05-29 14:39:02 +0900

    fix: ngrok설정 추가

--- src/api/base.ts
+++ src/api/base.ts
@@ -1,12 +1,20 @@
 // src/api/authenticatedApi.ts (세션 필요할 때)
 import axios from 'axios';
 
-export const authenticatedApi = axios.create({
-  baseURL: 'https://7ded-2001-e60-9275-ff7b-6817-bfe1-6346-a280.ngrok-free.app',
-  withCredentials: true,
-});
-
 export const publicApi = axios.create({
   baseURL: 'https://7ded-2001-e60-9275-ff7b-6817-bfe1-6346-a280.ngrok-free.app',
   withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json',
+    'ngrok-skip-browser-warning': '69420',
+  },
+});
+
+export const authenticatedApi = axios.create({
+  baseURL: 'https://7ded-2001-e60-9275-ff7b-6817-bfe1-6346-a280.ngrok-free.app',
+  withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json',
+    'ngrok-skip-browser-warning': '69420',
+  },
 });
